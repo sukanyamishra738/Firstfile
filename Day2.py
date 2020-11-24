394be0990c53d8a8a7cdf51b8fae4fbea45ed52d
sukanyamishra738 <56433148+sukanyamishra738@users.noreply.github.com>
2020-11-24 19:57:18 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,19 @@
+# No. 1
+a = b = c = 5
+print(a/10)
+print(b*50)
+print(c + 60)
+# No. 2
+s = "sanya"
+print(s.replace(s[2],"G"))
+# No. 3
+a = 10
+b = 5.5
+print("int value of a:", a)
+print("float value of b:", b)
+temp = a
+a = b
+b = temp
+print("Now the value of a:", a)
+print("Now the value of b:", b)
+
